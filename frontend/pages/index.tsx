87c84c384361b29ff13e485822d29630f31fe8ad
shintaro uchiyama <shintaro.uchiyama@jdsc.ai>
2021-01-28 00:35:35 +0900

    create google login component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,49 +3,7 @@ import firebase from "../hooks/firebase";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { AuthContext } from "../components/context/Auth";
-import styled from "styled-components";
-
-const GoogleSignIn = styled.div`
-  .google-btn {
-    width: 184px;
-    height: 42px;
-    background-color: #4285f4;
-    border-radius: 2px;
-    box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.25);
-  }
-  .google-icon-wrapper {
-    position: absolute;
-    margin-top: 1px;
-    margin-left: 1px;
-    width: 40px;
-    height: 40px;
-    border-radius: 2px;
-    background-color: #fff;
-  }
-  .google-icon {
-    position: absolute;
-    margin-top: 11px;
-    margin-left: 11px;
-    width: 18px;
-    height: 18px;
-  }
-  .btn-text {
-    float: right;
-    margin: 11px 11px 0 0;
-    color: #fff;
-    font-size: 14px;
-    letter-spacing: 0.2px;
-    font-family: "Roboto";
-  }
-  &:hover {
-    box-shadow: 0 0 6px #4285f4;
-  }
-  &:active {
-    background: #4285f4;
-  }
-
-  @import url(https://fonts.googleapis.com/css?family=Roboto:500);
-`;
+import GoogleSignIn from "../components/atoms/GoogleSignIn";
 
 const Home: FC = () => {
   const { currentUser } = useContext(AuthContext);
@@ -97,19 +55,7 @@ const Home: FC = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>Good Morning, World!</h1>
 
-        <GoogleSignIn className="container">
-          <div className="google-btn">
-            <div className="google-icon-wrapper">
-              <img
-                className="google-icon"
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-              />
-            </div>
-            <p className="btn-text">
-              <b>Sign in with google</b>
-            </p>
-          </div>
-        </GoogleSignIn>
+        <GoogleSignIn />
       </main>
     </div>
   );
