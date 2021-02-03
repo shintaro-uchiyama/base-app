5f8529ca740f55a5073ecd28da2911557053e3b7
shintaro uchiyama <shintaro.uchiyama@jdsc.ai>
2021-02-03 19:42:07 +0900

    auto detect i18next language

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,20 +3,10 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../components/theme";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-
 import { AuthProvider } from "../components/context/Auth";
+import "../i18n";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: "ja",
-    debug: true,
-    interpolation: { escapeValue: false },
-  });
-
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
