0baacd6cf290d48cef472fe8d3c3bf45b1b674d1
shintaro uchiyama <shintaro.uchiyama@corp.acall.jp>
2021-04-18 18:19:30 +0900

    fix(aws): import organization

--- infrastructure/aws/organization/main.tf
+++ infrastructure/aws/organization/main.tf
@@ -1,7 +1,15 @@
 # set default organization id
-data "aws_organizations_organization" "root" {}
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+  ]
+
+  feature_set = "ALL"
+}
+
 locals {
-  root_id = data.aws_organizations_organization.root.roots[0].id
+  root_id = aws_organizations_organization.org.roots[0].id
 }
 
 /*
@@ -24,6 +32,12 @@ resource aws_organizations_organizational_unit infra_production_organization_uni
   parent_id = aws_organizations_organizational_unit.infra_organization_unit.id
 }
 
+resource aws_organizations_account shared_services_production_account {
+  name = "shared-services-production"
+  email = "shintaro.a.uchiyama+shared-services-production@gmail.com"
+  parent_id = aws_organizations_organizational_unit.infra_production_organization_unit.id
+}
+
 # infra sdlc
 resource aws_organizations_organizational_unit infra_sdlc_organization_unit {
   name = "infra sdlc"
@@ -42,9 +56,9 @@ resource aws_organizations_organizational_unit security_production_organization_
   parent_id = aws_organizations_organizational_unit.security_organization_unit.id
 }
 
-resource aws_organizations_account security_production_account {
-  name = "shintaro.a.uchiyama+security-production@gmail.com"
-  email = "shintaro.a.uchiyama+security-production@gmail.com"
+resource aws_organizations_account log_archive_production_account {
+  name = "log-archive-production"
+  email = "shintaro.a.uchiyama+log-archive-production@gmail.com"
   parent_id = aws_organizations_organizational_unit.security_production_organization_unit.id
 }
 
@@ -74,10 +88,22 @@ resource aws_organizations_organizational_unit workload_production_organization_
   parent_id = aws_organizations_organizational_unit.workload_organization_unit.id
 }
 
+resource aws_organizations_account ucwork_production_account {
+  name = "ucwork-production"
+  email = "shintaro.a.uchiyama+ucwork-production@gmail.com"
+  parent_id = aws_organizations_organizational_unit.workload_production_organization_unit.id
+}
+
 # workload sdlc
 resource aws_organizations_organizational_unit workload_sdlc_organization_unit {
   name = "workload sdlc"
   parent_id = aws_organizations_organizational_unit.workload_organization_unit.id
+}
+
+resource aws_organizations_account ucwork_sdlc_account {
+  name = "ucwork-sdlc"
+  email = "shintaro.a.uchiyama+ucwork-sdlc@gmail.com"
+  parent_id = aws_organizations_organizational_unit.workload_sdlc_organization_unit.id
 }
 
 # sandbox
